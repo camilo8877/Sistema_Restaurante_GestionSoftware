700817cafd557c442b51b6be279ff6a426c90375
Camilo Ramirez <camilo8877@gmail.com>
2026-05-07 14:45:07 -0500

    Séptimo commit

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 
+import os
 from app import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # 1. Railway asigna un puerto dinámico, debemos leerlo
+    port = int(os.environ.get("PORT", 5000))
+    
+    # 2. '0.0.0.0' permite que la app sea visible fuera del contenedor
+    # 3. debug debe estar en False para producción
+    app.run(host='0.0.0.0', port=port, debug=False)
